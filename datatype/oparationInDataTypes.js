bb660564d0bfa9a666224340cc5a8c5bca532825
shuvronilmondal3 <shuvronilmondal53@gmail.com>
2024-02-29 18:49:19 +0530

    oparation
    oparetion in data type

---
+++ datatype/oparationInDataTypes.js
@@ -0,0 +1,23 @@
+//******** oparation in converted datatype  */
+let value = 90;
+let negValue = -value// negetive value asign
+// console.log(negValue);
+//2^3
+// console.log(2**3);
+// we can concate string 
+let str1 = "hello"
+let str2 = "jitu"
+let concate = str1+str2;
+// console.log(concate)
+
+console.log("1"+2+2)//->122
+console.log(1+2+"2")//->32
+//But why ??? --> which datatype is coming first all will treate as same
+let x = 3
+y=x++
+console.log(`x:${x},y:${y}`)// x=4 y=3
+
+let a=3
+let b=++a
+console.log(`a:${a},b:${b}`);
+
