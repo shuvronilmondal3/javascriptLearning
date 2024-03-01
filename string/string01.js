751e51fe623e7b7364ddc86238b728a19057f366
shuvronilmondal3 <shuvronilmondal53@gmail.com>
2024-03-01 14:08:23 +0530

    string

---
+++ string/string01.js
@@ -0,0 +1,17 @@
+let name = "Jitu ";
+const repo = 3
+// console.log(name + repo+" repo and going")// don't do this type of concatination this is old type
+//use this 
+// console.log(`Hello my name is ${name} and my repository count is ${repo}.`);// this mordern js
+// another type that how we can declear string
+let st = new String("Jitu")
+// console.log(st.length);
+// console.log(name. toUpperCase())
+
+// console.log(st.__proto__);//we can see in browser all function in the datatype that we can use 
+// slice() and substring() both are same and the difference is we can use negetive value in slice method
+let st1 = "   ki   "
+console.log(st1);
+console.log(st1.trim())//Removes the leading and trailing white space and line terminator characters from a string.
+
+//**************** next i have to learn about all the methods in string ********************************************* */
